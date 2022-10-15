385cf9ad67ba5298a78cb966ec79fd63734c9259
Jere Kuusela <jere.kuusela@iki.fi>
2022-10-15 11:52:37 +0300

    Server sync update.

--- SpawnerTweaks/SpawnerTweaks.cs
+++ SpawnerTweaks/SpawnerTweaks.cs
@@ -7,8 +7,14 @@ namespace SpawnerTweaks;
 public class Plugin : BaseUnityPlugin {
   const string GUID = "spawner_tweaks";
   const string NAME = "Spawner Tweaks";
-  const string VERSION = "1.3";
-  public static ServerSync.ConfigSync ConfigSync = new(GUID, NAME, VERSION);
+  const string VERSION = "1.4";
+  public static ServerSync.ConfigSync ConfigSync = new(GUID)
+  {
+    DisplayName = NAME,
+    CurrentVersion = VERSION,
+    IsLocked = true,
+    ModRequired = true
+  };
   public void Awake() {
     ConfigWrapper wrapper = new("spawner_config", Config, ConfigSync);
     Configuration.Init(wrapper);
