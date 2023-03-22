1487a4aca8807131f861cce3ab854880c9f72aa4
Jere Kuusela <jere.kuusela@iki.fi>
2023-03-22 18:56:48 +0200

    Adds texts for beehives.

--- SpawnerTweaks/Beehive.cs
+++ SpawnerTweaks/Beehive.cs
@@ -23,6 +23,20 @@ public class BeehivePatches
   // float,float,float (x,z,y)
   static int SpawnEffect = "override_spawn_effect".GetStableHashCode();
   // prefab,flags,variant,childTransform|prefab,flags,variant,childTransform|...
+  static int TextBiome = "override_text_biome".GetStableHashCode();
+  // string
+  static int TextSpace = "override_text_space".GetStableHashCode();
+  // string
+  static int TextSleep = "override_text_sleep".GetStableHashCode();
+  // string
+  static int TextHappy = "override_text_happy".GetStableHashCode();
+  // string
+  static int TextCheck = "override_text_check".GetStableHashCode();
+  // string
+  static int TextExtract = "override_text_extract".GetStableHashCode();
+  // string
+  static int Name = "override_name".GetStableHashCode();
+  // string
 
   [HarmonyPatch(nameof(Beehive.Awake)), HarmonyPostfix]
   static void Setup(Beehive __instance)
@@ -34,12 +48,18 @@ public class BeehivePatches
     if (!__instance.m_beeEffect) __instance.m_beeEffect = new();
     Helper.Int(view, MaxAmount, value => obj.m_maxHoney = value);
     Helper.Int(view, Biome, value => obj.m_biome = (Heightmap.Biome)value);
-    Helper.Int(view, SpawnCondition, value => obj.m_effectOnlyInDaylight = value == 1);
+    Helper.Int(view, SpawnCondition, value => obj.m_effectOnlyInDaylight = value != 1);
     Helper.Item(view, Spawn, value => obj.m_honeyItem = value);
     Helper.Float(view, Speed, value => obj.m_secPerUnit = value);
     Helper.Float(view, MaxCover, value => obj.m_maxCover = value);
     Helper.Offset(view, CoverOffset, obj.m_coverPoint, value => obj.m_coverPoint = value);
     Helper.Offset(view, SpawnOffset, obj.m_spawnPoint, value => obj.m_spawnPoint = value);
-    Helper.String(view, SpawnEffect, value => obj.m_spawnEffect = Helper.ParseEffects(value));
+    Helper.String(view, TextBiome, value => obj.m_areaText = value);
+    Helper.String(view, TextSpace, value => obj.m_freespaceText = value);
+    Helper.String(view, TextSleep, value => obj.m_sleepText = value);
+    Helper.String(view, TextHappy, value => obj.m_happyText = value);
+    Helper.String(view, TextCheck, value => obj.m_checkText = value);
+    Helper.String(view, TextExtract, value => obj.m_extractText = value);
+    Helper.String(view, Name, value => obj.m_name = value);
   }
 }
